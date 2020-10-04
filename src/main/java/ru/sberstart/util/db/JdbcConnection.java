8c7063488fbd958e1542b38b0d8bc0269bed7ac3
Artem Kostromin <kostromin14@mail.ru>
2020-10-04 19:47:10 +0300

    Take application properties from file.

--- src/main/java/ru/sberstart/util/db/JdbcConnection.java
+++ src/main/java/ru/sberstart/util/db/JdbcConnection.java
@@ -1,18 +1,37 @@
 package ru.sberstart.util.db;
 
+import lombok.SneakyThrows;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class JdbcConnection {
+    private final static Properties properties = new Properties();
 
+    @SneakyThrows
     public static Connection getConnection() {
+        FileInputStream fis = new FileInputStream("src/main/resources/application.properties");
+        properties.load(fis);
+
+        Connection connection = null;
+
+        String name = (String) properties.get("datasource.name");
+        String url = (String) properties.get("datasource.url");
+        String user = (String) properties.get("datasource.user");
+        String password = (String) properties.get("datasource.password");
+
         try {
-            Class.forName("org.h2.Driver");
-            return DriverManager.getConnection("jdbc:h2:~/Bank_API", "sa", "");
-        } catch (ClassNotFoundException | SQLException e) {
+            Class.forName(name);
+            return DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
         }
-        return null;
+        return connection;
     }
 }
