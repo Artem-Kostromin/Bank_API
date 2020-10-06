52ac6c69a6683175db0ab94a7e8a2d90237dfb77
Dmitriy Pyshinskiy <dmitriy.pyshinskiy@mail.ru>
2020-10-06 11:41:40 +0300

    writing tests

--- src/test/java/ru/sberstart/repository/TestAccountRepository.java
+++ src/test/java/ru/sberstart/repository/TestAccountRepository.java
@@ -13,10 +13,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Objects;
 
 public class TestAccountRepository {
     private final Connection connection = JdbcConnection.getConnection();
+    private final AccountRepository repo = new AccountRepositoryImpl(connection);
 
     @Before
     public void before() {
@@ -28,19 +30,29 @@ public class TestAccountRepository {
         } catch (SQLException | FileNotFoundException throwables) {
             throwables.printStackTrace();
         }
-
     }
 
     @Test
     public void findOneTest() throws SQLException {
         long id = 3;
-        Account account = new AccountRepositoryImpl(connection).findOne(id);
+        Account account = repo.findOne(id);
         Assert.assertEquals(account.getId(), id);
     }
 
     @Test
-    public void findAll() throws SQLException {
-
+    public void findAllTest() throws SQLException {
+        List<Account> accounts = repo.findAll();
+        Assert.assertTrue(accounts.size() > 0);
     }
 
+    @Test
+    public void persistTest() throws SQLException {
+        long id = repo.persist(new Account()).getId();
+        Assert.assertTrue(id > 0);
+    }
+
+    @Test
+    public void removeOneTest() throws SQLException {
+        Assert.assertTrue(repo.removeOne(2));
+    }
 }
