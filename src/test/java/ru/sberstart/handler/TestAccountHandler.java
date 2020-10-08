41aa2f6be4f28cccdc8d7e414dbfcdf69aefa517
Dmitriy Pyshinskiy <dmitriy.pyshinskiy@mail.ru>
2020-10-08 18:39:53 +0300

    adding new Test for AccountHandler

--- src/test/java/ru/sberstart/handler/TestAccountHandler.java
+++ src/test/java/ru/sberstart/handler/TestAccountHandler.java
@@ -1,24 +1,54 @@
 package ru.sberstart.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import ru.sberstart.handler.account.CreateAccountHandler;
-import ru.sberstart.repository.impl.AccountRepositoryImpl;
-import ru.sberstart.service.AccountService;
-import ru.sberstart.service.impl.AccountServiceImpl;
-import ru.sberstart.util.db.JdbcConnection;
-import sun.net.www.http.HttpClient;
+import ru.sberstart.entity.Account;
 
 import java.io.IOException;
-import java.net.URL;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class TestAccountHandler {
-    private final AccountService accountService = new AccountServiceImpl(new AccountRepositoryImpl(JdbcConnection.getConnection()));
-    private final CreateAccountHandler createAccountHandler = new CreateAccountHandler(accountService);
+    private final CloseableHttpClient client = HttpClients.createDefault();
+
+    @Before
+    public void initMockServer() throws IOException {
+
+    }
 
     @Test
     public void createAccountHandlerTest() throws IOException {
-        HttpClient httpClient = HttpClient.New(new URL("http://localhost:8080/createAccount"));
-        httpClient.
-        createAccountHandler.handle(httpClient);
+        HttpGet request = new HttpGet("http://localhost:8080/createAccount");
+        CloseableHttpResponse response = client.execute(request);
+        HttpEntity entity = response.getEntity();
+        InputStream is = entity.getContent();
+
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(is, Account.class);
+
+        Assert.assertTrue(account.getId() > 0);
+    }
+
+    @Test
+    public void getAccountsHandlerTest() throws IOException {
+        HttpGet request = new HttpGet("http://localhost:8080/getAccounts");
+        CloseableHttpResponse response = client.execute(request);
+        HttpEntity entity = response.getEntity();
+        InputStream is = entity.getContent();
+        String result = IOUtils.toString(is, StandardCharsets.UTF_8);
+        System.out.println(result);
+
+        ObjectMapper mapper = new ObjectMapper();
+        Account[] accounts = mapper.readValue(result, Account[].class);
+
+        Assert.assertTrue(accounts.length > 1);
     }
 }
